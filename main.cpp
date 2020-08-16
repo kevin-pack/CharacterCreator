d0f2c563ce9e215eee7097f5033abe9fbdaa2c32
Kevin Pack <kapack@ucdavis.edu>
2020-08-16 15:57:49 -0700

    more changes

--- main.cpp
+++ main.cpp
@@ -19,37 +19,85 @@ void getInput(std::string &input){
     toLower(input);
 }
 
+bool isNumeric(const std::string &s){
+    for(auto i : s){
+        if (!isdigit(i)){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 int main(){ 
     std::cout << "\nWelcome to Kevin's Character Creator\n"; 
     std::cout << "------------------------------------\n";
-    std::cout << "enter exit, quit, or q to quit\n\n"; 
+    std::cout << "enter exit... if u want to exit\n\n"; 
 
     std::string input;
+
     do{
-        std::cout << "Create new character or edit ?\n";
+        std::cout << "Create new character or edit existing character?\n";
         getInput(input);
-        if(input.substr(0,4) == "new"){
-            std::cout << "Point buy or roll for abilities?\n";
-            getInput(input);
 
-            // point buy
-            if(input.substr(0,5) == "point" || input.substr(0,4) == "buy"){
-                std::cout << "How many points are you alloted?\n";
-                getInput(input);
-                size_t points;
-                std::stringstream(input) >> points;
-                std::cout << points << std::endl;
-            }
+        // new character
+        if(input.substr(0,4) == "new" || input.substr(0,6) == "create"){
+            // get name
+            do{
 
-            // roll for abilities
-            else if(input.substr(0,4) == "roll"){
-                std::cout << "ROLL\n";
-            }
+            }while(input!="exit");
 
-            else{
-                std::cout << "Input not understood. Try again.\n";
-            }
+            if(input!="exit"){
+                // point buy or roll
+                do{
+                    std::cout << "Point buy or roll for abilities?\n";
+                    getInput(input);
+
+                    // point buy
+                    if(input.substr(0,5) == "point" || input.substr(0,4) == "buy"){
+                        std::cout << "\nLow Fantasy:      10\n";
+                        std::cout << "Standard Fantasy: 15\n";
+                        std::cout << "High Fantasy:     20\n";
+                        std::cout << "Epic Fantasy:     25\n\n"; 
+                        std::cout << "How many points are you allotted?\n";
+
+                        // # of points input
+                        do{
+                            getInput(input);
+                            if(isNumeric(input)){
+                                size_t points;
+                                std::stringstream(input) >> points;
+                            }
+                            else{
+                                std::cout << "Invalid input\n";
+                            }
+                            
+                        } while(input != "exit");
+                    } // point buy 
+
+                    // roll for abilities
+                    else if(input.substr(0,4) == "roll"){
+                        std::cout << "ROLL\n";
+                    } // roll for abilities
+
+                    // bad input
+                    else if(input != "exit"){
+                        std::cout << "Invalid input\n";
+                    }
+                } while(input != "exit"); // point buy or roll? 
+            } // input!=exit
+        } // new character
+
+        // edit character
+        else if(input.substr(0,4) == "edit"){
+            // TODO
+        } // edit character
+
+        else if(input != "exit"){
+            std::cout << "Invalid input\n\a";
         }
-    }while(input != "exit" && input != "quit" && input != "q");
+    }while(input != "exit");
+
     return 0;
 }
